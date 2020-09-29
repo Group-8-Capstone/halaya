461288d15d719b83b5865d1b26d8ff58d6b2012f
Heavenjes <paulinecalatrava@gmail.com>
2020-09-29 20:17:25 +0800

    added stock

--- src/views/Stock.vue
+++ src/views/Stock.vue
@@ -1,6 +1,5 @@
 <template>
   <v-card class="ma-5 mb-12 pa-5">
-  
     <v-card-title>
       Stock
       <v-spacer></v-spacer>
@@ -15,10 +14,9 @@
       <v-list>
         <v-btn class="ma-5" color="purple darken-2" rounded outlined dark @click="showDialog">
           <v-icon>mdi-plus</v-icon>
-          <v-toolbar-title>Add Stock</v-toolbar-title>
+          <v-toolbar-title>Add Stock Ingredients</v-toolbar-title>
         </v-btn>
       </v-list>
-
       <v-dialog v-model="stockDialog" width="400px">
         <v-card>
           <v-spacer></v-spacer>
@@ -28,48 +26,23 @@
           <v-container>
             <v-row class="mx-2">
               <v-col cols="12">
-                <v-text-field
-                  prepend-icon="mdi-account-outline"
-                  placeholder="Kilo of Ube"
-                  v-model="ubeKilo"
-                  :error-messages="ubeKiloErrors"
-                  @input="$v.ubeKilo.$touch()"
-                  @blur="$v.ubeKilo.$touch()"
-                  type=number
-                  required
-                ></v-text-field>
+                <v-select
+                v-model="ingredientsName"
+                :items="items"
+                label="Ingredients"
+                ></v-select>
               </v-col>
               <v-col cols="12">
-                <v-menu
-                  ref="addDateMenu"
-                  v-model="addDateMenu"
-                  :close-on-content-click="false"
-                  :return-value.sync="date"
-                  transition="scale-transition"
-                  offset-y
-                  min-width="290px"
-                >
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-text-field
-                      v-model="deliveryDate"
-                      label="Picker in menu"
-                      prepend-icon="mdi-calendar"
-                      readonly
-                      v-bind="attrs"
-                      v-on="on"
-                    ></v-text-field>
-                  </template>
-                  <v-date-picker
-                    v-model="deliveryDate"
-                    :min="deliveryDate"
-                    :max="getEndDate"
-                    color="deep-purple lighten-1"
-                    no-title
-                    scrollable
-                  >
-                    <v-spacer></v-spacer>
-                  </v-date-picker>
-                </v-menu>
+                 <v-text-field
+                  prepend-icon="mdi-plus"
+                    min="1"
+                    type="number"
+                    placeholder="ingredients unit"
+                    v-model="ingredientsUnit"
+                    :error-messages="ingredientsUnitErrors"
+                    @input="$v.ingredientsUnit.$touch()"
+                    @blur="$v.ingredientsUnit.$touch()"
+                ></v-text-field>
               </v-col>
             </v-row>
           </v-container>
@@ -81,14 +54,61 @@
         </v-card>
       </v-dialog>
     </v-card-title>
+     <v-row class="mx-2">
+       <v-col cols="12">
     <v-data-table
       :headers="headers"
       :items="stocks"
       :search="search"
-    ></v-data-table>
+    >
+     <template v-slot:item.action="{ item }">
+          <v-icon
+            @click="editDialog = !editDialog, editItem(item) "
+            class="mr-2"
+            normal
+            title="Edit"
+          >mdi-table-edit</v-icon>
+        </template></v-data-table>
+    </v-col>
+    <v-col cols="8">
+      <v-card  
+      class="mx-auto"
+      outlined>
+    <v-card-title>Check Stock</v-card-title>
+      <v-simple-table height="200px">
+    <template v-slot:default>
+      <thead>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Expected Stock Output</td>
+          <td>{{expectedProduct}}</td>
+        </tr>
+        <tr>
+          <td>Total Order</td>
+          <td>{{sumOrder}}</td>
+        </tr>
+        <tr
+        v-bind:class='{ "enough": stockAvailability == "Enough", "need": stockAvailability == "Not enough" }'>
+          <td>Status Order</td>
+          <td>{{stockAvailability}}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+  </v-card>
+  </v-col>
+     </v-row>
   </v-card>
 </template>
-
+<style>
+.enough{
+  background-color: lightskyblue;
+}
+.need{
+  background-color: lightcoral;
+}
+</style>
 <script>
 import axios from "axios";
 import Swal from "sweetalert2";
@@ -103,63 +123,49 @@ import {
     name: "Stock",
     data () {
       return {
+      sumOrder:null,
+      expectedProduct:null,
+      stockAvailability:null,
       search: '',
-      addDateMenu: false,
-      date2: new Date(),
+      ingredientsUnit:'',
+      ingredientsName:'',
       stockStatus:"Enough",
-      search: "",
-      date: new Date().toISOString().substr(0, 10),
-      deliveryDate: new Date().toISOString().substr(0, 10),
+      search: "", 
       ubeKilo: "",
       stocks:[],
+       items: ['Ube', 'Condensed milk', 'Butter', 'Sugar','Coconut milk'],
       stockDialog: false,
         headers: [
-          { text: 'Date', value: 'delivery_date' },
+          { text: 'Ingredients', value: 'ingredients_name' },
           {
-            text: 'Kilo of Ube',
+            text: 'Ingredients Unit',
             align: 'start',
-            sortable: false,
-            value: 'ube_kilo',
+            sortable: true,
+            value: 'ingredients_unit',
           },
-          { text: 'Expected Output', value: 'expected_output' },
-          { text: 'Ordered Quantity', value: 'fat' },
-          { text: 'Status', value: 'stock_status' },
-        
+          { text: 'Status', value: 'ingredients_status' },
+          { text: "Actions", value: "action", sortable: false },
         ],
       }},
        validations: {
-    ubeKilo: {
+    ingredientsUnit: {
       required
     },
   },
 
   computed: {
-    ubeKiloErrors() {
+    ingredientsUnitErrors() {
       const errors = [];
-      if (!this.$v.ubeKilo.$dirty) return errors;
-      !this.$v.ubeKilo.required && errors.push("Kilo of ube is required.");
+      if (!this.$v.ingredientsUnit.$dirty) return errors;
+      !this.$v.ingredientsUnit.required && errors.push("unit is required.");
       return errors;
     },
-
-    getEndDate() {
-      var endDate = new Date(
-        this.date2.getFullYear(),
-        this.date2.getMonth() + 1,
-        10
-      );
-      return endDate.toISOString().slice(0, 10);
-    },
-    getEndDate() {
-      var endDate = new Date(
-        this.date2.getFullYear(),
-        this.date2.getMonth() + 1,
-        10
-      );
-      return endDate.toISOString().slice(0, 10);
-    }
   },
   created() {
       this.fetchStock();
+      this.postExpectedProduct();
+      this.postSumOrder();
+      this.checkStatus();
       setInterval(this.fetchStock(),3000)
     },
   
@@ -168,7 +174,23 @@ import {
     showDialog() {
       this.stockDialog = !this.stockDialog
     },
+    postExpectedProduct(){
+      axios.get("http://127.0.0.1:8000/api/fetch/expectedProduct").then(response=>{
+        this.expectedProduct=response.data;
+        this.checkStatus();
+      })
+    },
+    postSumOrder(){
+      axios.get("http://127.0.0.1:8000/api/fetch/sumOrder").then(response=>{
+        this.sumOrder=response.data;
 
+      })
+    },
+    checkStatus(){
+       axios.get("http://127.0.0.1:8000/api/fetch/stockStatus").then(response=>{
+        this.stockAvailability=response.data;
+      })
+    },
     fetchStock(){
       axios.get("http://127.0.0.1:8000/api/fetch/stock").then(response=>{
         console.log(response.data)
@@ -178,34 +200,17 @@ import {
 
     addStock(){
       let  newStock = {
-        ubeKilo:this.ubeKilo,
-        deliveryDate:this.deliveryDate,
+        ingredientsName:this.ingredientsName,
+        ingredientsUnit:this.ingredientsUnit,
         stockStatus:this.stockStatus,
       }
       console.log(newStock)
       axios.post("http://127.0.0.1:8000/api/create/stock", newStock)
       .then(response=>{
-        if(response.data.message==="already"){
-        Swal.fire({
-        title: "Stocks for this date is already added",
-        icon: "warning",
-        confirmButtonColor: "#3085d6",
-        confirmButtonText: "Okay",
-        reverseButtons: true
-      })
-      }else if (response.data.message==="not"){
-        Swal.fire({
-        title: "Successfully Added!",
-        icon: "success",
-        confirmButtonColor: "#3085d6",
-        confirmButtonText: "Okay",
-        reverseButtons: true
-      })
       this.stockDialog = false
-      }
       this.fetchStock();
+      this.postExpectedProduct()
     })
-    
   }
   }}
 </script>
