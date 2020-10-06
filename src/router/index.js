c74aec9d64c80bb9efdf677e1fa1d049af7ca991
junnard.rivera@gmail.com <junnard.rivera@gmail.com>
2020-10-06 14:42:12 +0800

    Delivery order layout done

--- src/router/index.js
+++ src/router/index.js
@@ -4,15 +4,9 @@ import VueRouter from "vue-router";
 import Order from "../views/Order.vue";
 import Stock from "../views/Stock.vue";
 import Delivery from "../views/DeliveryOrder.vue";
-<<<<<<< HEAD
-import Login from "../views/Login.vue";
-import Delivered from "../views/Delivered.vue";
-import DeliveryDetails from "../views/DeliveryDetails";
-=======
 import Login from "../views/Login.vue"
 import Delivered from "../views/Delivered.vue";
 import Home from "../views/Home.vue";
->>>>>>> 5b9fe04be80e58d8f0439320dbfa2565122ea7ff
 
 
 
@@ -22,53 +16,15 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
 
-<<<<<<< HEAD
-routes : [
-  {
-    path: "/login",
-    name: "Login",
-    component: Login
-  },
-  {
-    path: "/order",
-    name: "Order",
-    component: Order
-  },
-  {
-    path: "/stock",
-    name: "Stock",
-    component: Stock
-  },
-  {
-    path: "/delivered",
-    name: "Delivered",
-    component: Delivered
-  },
-  {
-    path: "/delivery",
-    name: "Delivery",
-    component: Delivery
-  },
-  {
-    path: "deliveryDetails",
-    name: "DeliveryDetails",
-    component: DeliveryDetails
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-]
-=======
   routes: [{
       path: "/login",
       name: "Login",
       component: Login
+    },
+    {
+      path: "/",
+      name: "Home",
+      component: Home
     },
     {
       path: "/order",
@@ -105,7 +61,6 @@ routes : [
         import( /* webpackChunkName: "about" */ "../views/About.vue")
     }
   ]
->>>>>>> 5b9fe04be80e58d8f0439320dbfa2565122ea7ff
 
 });
 
--- src/views/DeliveryOrder.vue
+++ src/views/DeliveryOrder.vue
@@ -1,81 +1,84 @@
 <template>
-  <v-card :loading="loading" class="mx-auto my-12" max-width="374" max-height="580">
-    <template slot="progress">
-      <v-progress-linear color="deep-purple" height="10" indeterminate></v-progress-linear>
-    </template>
+  <div>
     <v-row>
-      <v-col v-for="(delivery, index) in deliveryOrders" :key="index">
-        <v-img height="100%" width="100%" src="../assets/ubeCard.jpg"></v-img>
-        <v-card-title>Wawen's Ube Halaya</v-card-title>
-        <v-card-text>
-          <div>100% Pure Ube</div>
-        </v-card-text>
-        <v-divider class="mx-4"></v-divider>
-        <v-card-title>{{ delivery.title }}</v-card-title>
-        <v-card-text>
-          <div>Five delivery locations for this round of delivery.</div>
-          <v-chip-group v-model="selection" active-class="deep-purple accent-4 white--text" column>
-            <v-chip>{{ delivery.distance1 }}</v-chip>
-            <v-chip>{{ delivery.distance2 }}</v-chip>
-            <v-chip>{{ delivery.distance3 }}</v-chip>
-            <v-chip>{{ delivery.distance4 }}</v-chip>
-            <v-chip>{{ delivery.distance5 }}</v-chip>
-          </v-chip-group>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="deep-purple lighten-2" text @click="reserve">View Delivery Details</v-btn>
-        </v-card-actions>
+      <v-col v-for="(item, index) in deliveryOrders" :key="index">
+        <v-card :loading="loading" class="mx-auto my-2" max-width="310">
+          <v-progress-linear color="deep-purple" height="8" indeterminate></v-progress-linear>
+          <v-img height="100%" width="100%" src="../assets/ubeCard.jpg"></v-img>
+          <v-card-title>Wawen's Ube Halaya</v-card-title>
+          <v-divider class="mx-4"></v-divider>
+          <v-card-title>{{item.title}}</v-card-title>
+          <v-card-text>
+            <div>Five delivery locations for this round of delivery.</div>
+            <v-chip-group
+              v-model="selection"
+              active-class="deep-purple accent-4 white--text"
+              column
+            >
+              <v-chip>{{item.distance1}}</v-chip>
+              <v-chip>{{item.distance2}}</v-chip>
+              <v-chip>{{item.distance3}}</v-chip>
+              <v-chip>{{item.distance4}}</v-chip>
+              <v-chip>{{item.distance5}}</v-chip>
+            </v-chip-group>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="deep-purple lighten-2" text @click="reserve">View Delivery Details</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-col>
     </v-row>
-  </v-card>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
   data() {
-    deliveryOrders: [
-      {
-        title: "Delivery 1",
-        distance1: "0.5 km",
-        distance2: "0.8 km",
-        distance3: "1 km",
-        distance4: "1 km",
-        distance5: "1.1 km"
-      },
-      {
-        title: "Delivery 2",
-        distance1: "1.1 km",
-        distance2: "1.2 km",
-        distance3: "1.3 km",
-        distance4: "1.5 km",
-        distance5: "1.5 km"
-      },
-      {
-        title: "Delivery 3",
-        distance1: "1.5 km",
-        distance2: "1.5 km",
-        distance3: "1.5 km",
-        distance4: "1.6 km",
-        distance5: "1.6 km"
-      },
-      {
-        title: "Delivery 4",
-        distance1: "1.6 km",
-        distance2: "1.7 km",
-        distance3: "1.7 km",
-        distance4: "1.8 km",
-        distance5: "1.8 km"
-      },
-      {
-        title: "Delivery 5",
-        distance1: "1.8 km",
-        distance2: "1.8 km",
-        distance3: "1.9 km",
-        distance4: "1.9 km",
-        distance5: "1.2 km"
-      }
-    ];
+    return {
+      deliveryOrders: [
+        {
+          title: "Delivery 1",
+          distance1: "0.5 km",
+          distance2: "0.8 km",
+          distance3: "1 km",
+          distance4: "1 km",
+          distance5: "1.1 km"
+        },
+        {
+          title: "Delivery 2",
+          distance1: "1.1 km",
+          distance2: "1.2 km",
+          distance3: "1.3 km",
+          distance4: "1.5 km",
+          distance5: "1.5 km"
+        },
+        {
+          title: "Delivery 3",
+          distance1: "1.5 km",
+          distance2: "1.5 km",
+          distance3: "1.5 km",
+          distance4: "1.6 km",
+          distance5: "1.6 km"
+        },
+        {
+          title: "Delivery 4",
+          distance1: "1.6 km",
+          distance2: "1.7 km",
+          distance3: "1.7 km",
+          distance4: "1.8 km",
+          distance5: "1.8 km"
+        },
+        {
+          title: "Delivery 5",
+          distance1: "1.8 km",
+          distance2: "1.8 km",
+          distance3: "1.9 km",
+          distance4: "1.9 km",
+          distance5: "1.2 km"
+        }
+      ]
+    };
   }
 };
 </script>
