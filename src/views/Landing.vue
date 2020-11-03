040b070846c42db550531e2177762e69c5b76553
junnard.rivera@gmail.com <junnard.rivera@gmail.com>
2020-11-03 20:50:12 +0800

    landing

---
+++ src/views/Landing.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      <v-list-item-avatar size="60">
+        <img src="../assets/wawens.png">
+      </v-list-item-avatar>
+      <v-toolbar-title class="deep-purple--text">WAWEN'S UBE HALAYA</v-toolbar-title>
+    </v-app-bar>
+  </v-app>
+</template>
---
+++ src/views/MyOrder.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <v-tabs v-model="tabs" right color="deep-purple accent-4">
+            <v-tab>On Order</v-tab>
+            <v-tab>Past</v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="tabs">
+            <v-tab-item>
+                <v-card>
+                    <v-data-table :headers="headers" :items="myOrders" :search="search">
+                    </v-data-table>
+                </v-card>
+            </v-tab-item>
+        </v-tabs-items>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name: 'MyOrders',
+    data() {
+        return {
+            myOrders: [],
+            search: '',
+            headers: [
+                {text: "Product Name", value: "", sortable: false},
+                {text: "Quantity", value: "", sortable: false},
+                {text: "Delivery Address", value: "", sortable: false},
+                {text: "Delivery Date", value: "", sortable: false},
+                ]
+        }
+    }
+}
+</script>
