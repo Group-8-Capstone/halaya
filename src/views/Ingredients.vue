1d021186daa235a8948204f9125d77f8255115f2
junnard.rivera@gmail.com <junnard.rivera@gmail.com>
2020-11-16 22:37:11 +0800

    pdf for other logs added

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -54,7 +54,7 @@
     <v-card id="cardtable" class="ma-5 mb-12 pa-5">
       <v-row>
         <v-spacer></v-spacer>
-        <Pdf :headers="headers" :displayIngredientsRecords="displayIngredientsRecords" class="float-right mr-5"></Pdf>
+        <IngredientsPdf :headers="headers" :displayIngredientsRecords="displayIngredientsRecords" class="float-right mr-5"></IngredientsPdf>
       </v-row>
       <v-card-title>
         Recorded Used Ingredients
